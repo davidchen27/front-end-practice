b4733be8aa51f9045497d49a737b5dbf9cb74891
davidchen27 <davidchen27@users.noreply.github.com>
2023-09-27 05:59:52 +0000

    deploy: dfb132f32dbcbc37706fdcdaa5a937f55b41afa4

--- deploy.js
+++ deploy.js
@@ -7,10 +7,7 @@ const run = () => {
     const aNum = parseInt(a.match(reg)[0])
     const bNum = parseInt(b.match(reg)[0])
     return aNum - bNum
-  }).map(dir => {
-    if(!/^\d(.*)?$/.test(dir)) return
-    return `<li><a href="${dir}">${dir}</a></li>`
-  }).join('')
+  }).map(dir => `<li><a href="${dir}">${dir}</a></li>`).join('')
   const html = `
   <html><head><title>目录列表</title><style>ul{list-style: none;padding: 0;}li{width: auto;margin: 16px 0;text-align: center;border-radius: 5px;border: 1px solid #eee;transition: .3s;}li:hover{background: #eee;}a{display: block;padding: 10px;text-decoration: none;color: #333;}</style></head><body><ul>${ul}</ul></body></html>`
   fs.writeFileSync('index.html', html)
