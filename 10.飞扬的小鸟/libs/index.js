2ce8cd0c97c2b392ebc4fed3d727841cc2184190
chenwenjie <davidchen27@foxmail.com>
2023-11-10 17:42:45 +0800

    临时提交

--- 10.飞扬的小鸟/libs/index.js
+++ 10.飞扬的小鸟/libs/index.js
@@ -1,1 +1,12 @@
-const canvasEl = document.getElementById('canvas')
+class FlappyBird {
+  constructor(selector) {
+    this._canvasEl = document.querySelector(selector)
+    this._ctx = this._canvasEl.getContext('2d')
+    this._initPannel()
+  }
+  /** 初始化游戏面板 */
+  _initPannel() {
+    this._canvasEl.width = 600
+    this._canvasEl.height = 400
+  }
+}
