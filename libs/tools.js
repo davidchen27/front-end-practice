00b19ad25d7626ab37d0c175741e150dd090ae76
davidchen27 <davidchen27@users.noreply.github.com>
2024-09-06 09:49:32 +0000

    deploy: b56b86b79db94e57d985918f17508bbfc4f97415

--- libs/tools.js
+++ libs/tools.js
@@ -6,8 +6,8 @@
  * @returns 生成的随机数
  */
 function getRandomNum(min, max, withDecimals = false) {
-  const number = Math.random() * (max - min + 1) + min
-  return withDecimals ? number : Math.floor(number)
+  const number = Math.random() * (max - min + 1) + min;
+  return withDecimals ? number : Math.floor(number);
 }
 
 /**
@@ -15,7 +15,7 @@ function getRandomNum(min, max, withDecimals = false) {
  * @returns 生成的随机颜色
  */
 function getRandomColor() {
-  return `rgb(${getRandomNum(0, 255)}, ${getRandomNum(0, 255)}, ${getRandomNum(0, 255)})`
+  return `rgb(${getRandomNum(0, 255)}, ${getRandomNum(0, 255)}, ${getRandomNum(0, 255)})`;
 }
 
 /**
@@ -23,7 +23,29 @@ function getRandomColor() {
  * @returns 生成的随机字符
  */
 function getRandomChar() {
-  return String.fromCharCode(getRandomNum(0x30a0, 0x30ff))
+  return String.fromCharCode(getRandomNum(0x30a0, 0x30ff));
 }
 
-window._ = { getRandomNum, getRandomColor, getRandomChar }
+/**
+ * 防抖函数
+ * @param {Function} fn 需要防抖的函数
+ * @param {number} delay 防抖的延迟时间
+ * @param {boolean} immediate 是否立即执行
+ * @returns 防抖后的函数
+ */
+function debounce(fn, delay = 500, immediate = false) {
+  let timer = null;
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    if (immediate && !timer) {
+      timer = 10086;
+      return fn.apply(this, args);
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null;
+    }, delay);
+  }
+}
+
+window._ = { getRandomNum, getRandomColor, getRandomChar, debounce };
